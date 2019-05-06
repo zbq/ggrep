f9b3f0830b04bc08c21a7c17bd8ec23e0a92d1e2
zbq <zhengbaiqiang@gmail.com>
2019-05-06 20:10:11 +0800

    initial

---
+++ ggrep.el
@@ -0,0 +1,51 @@
+;;; ggrep.el --- grep with Graphical User Interface
+
+;; Copyright (C) 2019 zbq
+;; Repository: https://github.com/zbq/ggrep
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; For a full copy of the GNU General Public License
+;; see <http://www.gnu.org/licenses/>.
+
+(require 'widget)
+(eval-when-compile (require 'wid-edit))
+
+(defvar-local ggrep-lines nil)
+
+(defun ggrep-prev-line ()
+  )
+
+(defun ggrep-next-line ()
+  )
+
+(defun ggrep-collapse-line ()
+  )
+
+(defun ggrep-collapse-all ()
+  )
+
+(defun ggrep-expand-line ()
+  )
+
+(defun ggrep-expand-all ()
+  )
+
+(defun ggrep-open ()
+  )
+
+(defun ggrep-grep ()
+  (interactive))
+
+(defun ggrep-mode ()
+  (interactive))
+
+(provide 'ggrep)
